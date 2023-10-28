0cd23859630b627f56b28be42d96a956ad97a2e9
AndreySavchenkov <sannndvik16@gmail.com>
2023-10-28 16:26:18 +0200

    feat: added storage service

--- weather.js
+++ weather.js
@@ -1,22 +1,35 @@
 #!/usr/bin/env node
 
 import {getArgs} from "./helpers/args.js";
-import {printHelp} from "./services/log.service.js";
+import {printHelp, printSuccess, printError} from "./services/log.service.js";
 import {saveKeyValue} from "./services/storage.service.js";
 
-const initCli = () => {
-    const args = getArgs(process.argv);
+const saveToken = async (token) => {
+  if(!token.length) {
+    printError('Don\'t have token');
+    return;
+  }
+  try {
+    await saveKeyValue('token', token);
+    printSuccess('Token saved');
+  } catch (e) {
+    printError(e.message);
+  }
+};
 
-    if(args.h) {
-        printHelp()
-    }
-    if(args.c) {
-        //save city
-    }
-    if(args.t) {
-        saveKeyValue('token', args.t);
-        //save token
-    }
+const initCli = () => {
+  const args = getArgs(process.argv);
+
+  if (args.h) {
+    printHelp();
+  }
+  if (args.c) {
+    //save city
+  }
+  if (args.t) {
+    return saveToken(args.t);
+    //save token
+  }
 }
 
 initCli();
